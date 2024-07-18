bb34f5ff397133ad4867e6e144f0117e90549a04
Ömer Doğan <bycycomr@gmail.com>
2024-07-18 07:29:08 +0300

    chore: Update CSS and add Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,61 @@
 import React from "react";
 import { Flex, Menu } from "antd";
-import {FaLeaf  } from 'react-icons/fa';
+import { FaLeaf } from "react-icons/fa";
+import {
+  UserOutlined,
+  ProfileOutlined,
+  LoginOutlined,
+  OrderedListOutlined,
+  CarryOutOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 const Sidebar = () => {
   return (
     <>
       <Flex align="center" justify="center">
         <div className="logo">
-          <FaLeaf />  
+          <FaLeaf />
         </div>
       </Flex>
-      <Menu mode='inline' defaultSelectedKeys={['1']} className="menu-bar"/> 
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        className="menu-bar"
+        items={[
+          {
+            key: "1",
+            icon: <UserOutlined />,
+            label: "Dashboard",
+          },
+
+          {
+            key: "2",
+            icon: <ProfileOutlined />,
+            label: "My Orders",
+          },
+          {
+            key: "3",
+            icon: <LoginOutlined />,
+            label: "ToDo",
+          },
+          {
+            key: "4",
+            icon: <OrderedListOutlined />,
+            label: "Dashboard",
+          },
+          {
+            key: "5",
+            icon: <CarryOutOutlined />,
+            label: "Dashboard",
+          },
+          {
+            key: "6",
+            icon: <SettingOutlined />,
+            label: "Dashboard",
+          }
+        ]}
+      />
     </>
   );
 };
