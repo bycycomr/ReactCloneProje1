fe6bcafc7ce812462ea57377456da79aaabaef29
Ömer Doğan <bycycomr@gmail.com>
2024-07-19 16:45:06 +0300

    feat: Refactor Activity component to use consistent naming and formatting

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,33 +1,32 @@
-import { Flex, Typography, Button, List, Avatar
-} from "antd";
+import { Flex, Typography, Button, List, Avatar } from "antd";
 import React from "react";
 
 const data = [
   {
     name: "Emma Turner",
-    orderTime: "2",
+    orderTime: 2,
   },
   {
     name: "Liam Emma",
-    orderTime: "3",
+    orderTime: 3,
   },
   {
     name: "Olivia Reed",
-    orderTime: "4",
+    orderTime: 4,
   },
   {
     name: "Ethan Hayes",
-    orderTime: "5",
+    orderTime: 5,
   },
   {
     name: "Ava Simmons",
-    orderTime: "6",
+    orderTime: 6,
   },
 ];
 
 const Activity = () => {
   return (
-    <Flex direction="vertical" gap="small">
+    <Flex vertical gap="small">
       <Flex align="center" justify="space-between" gap="large">
         <Typography.Title level={3} strong className="primary--color">
           Recent Activity
@@ -36,24 +35,23 @@ const Activity = () => {
           View All
         </Button>
       </Flex>
-      <List 
-        pagination 
-        dataSource={data} 
+      <List
+        pagination
+        dataSource={data}
         renderItem={(user, index) => (
           <List.Item>
             <List.Item.Meta
               avatar={
-              <Avatar 
-              src='https://api.dicebar.com/7.x/miniavs/svg?seed=${index}'
-              />
-            }
+                <Avatar
+                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                />
+              }
               title={<a href="#">{user.name}</a>}
               description="Ordered a new plant"
-  
             ></List.Item.Meta>
 
-            <span>
-              {user.orderTime} {user.orderTime === 1 ? "day ago" : "days ago"} 
+            <span className="grey--color">
+              {user.orderTime} {user.orderTime === 1 ? "day ago" : "days ago"}
             </span>
           </List.Item>
         )}
